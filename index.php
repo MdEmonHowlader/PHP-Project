b438b3f8e03ee225c0314b5e8cf85e6b04ea079c
Emon <emon22205101732@diu.edu.bd>
2023-09-03 01:55:19 +0600

    update

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include_once'inc/slider.php';
 
 ?>
 
-    
       <!-- END section -->
 
       <section class="site-section py-sm">
@@ -161,22 +160,13 @@ include_once'inc/slider.php';
                 </form>
               </div>
               <!-- END sidebar-box -->
-              <div class="sidebar-box">
-                <div class="bio text-center">
-                  <img src="images/person_1.jpg" alt="Image Placeholder" class="img-fluid">
-                  <div class="bio-body">
-                    <h2>David Craig</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem facilis sunt repellendus excepturi beatae porro debitis voluptate nulla quo veniam fuga sit molestias minus.</p>
-                    <p><a href="#" class="btn btn-primary btn-sm rounded">Read my bio</a></p>
-                    <p class="social">
-                      <a href="#" class="p-2"><span class="fa fa-facebook"></span></a>
-                      <a href="#" class="p-2"><span class="fa fa-twitter"></span></a>
-                      <a href="#" class="p-2"><span class="fa fa-instagram"></span></a>
-                      <a href="#" class="p-2"><span class="fa fa-youtube-play"></span></a>
-                    </p>
-                  </div>
-                </div>
-              </div>
+
+              <!-- START Sidebar -->
+              <?php
+              include_once'inc/sidebar1.php';
+              
+              ?>
+              
               <!-- END sidebar-box -->  
               <div class="sidebar-box">
                 <h3 class="heading">Popular Posts</h3>
