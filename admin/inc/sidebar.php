a28de54bac252bfc928b7d7a295ed005495df64b
Emon <emon22205101732@diu.edu.bd>
2023-09-03 01:42:38 +0600

    update

---
+++ admin/inc/sidebar.php
@@ -0,0 +1,169 @@
+ <!-- ========== Left Sidebar Start ========== -->
+ <div class="vertical-menu">
+
+<!-- LOGO -->
+<div class="navbar-brand-box">
+    <a href="index.html" class="logo logo-dark">
+        <span class="logo-sm">
+            <img src="assets/images/logo-sm.png" alt="" height="22">
+        </span>
+        <span class="logo-lg">
+            <img src="assets/images/logo-dark.png" alt="" height="20">
+        </span>
+    </a>
+
+    <a href="index.html" class="logo logo-light">
+        <span class="logo-sm">
+            <img src="assets/images/logo-sm.png" alt="" height="22">
+        </span>
+        <span class="logo-lg">
+            <img src="assets/images/logo-light.png" alt="" height="20">
+        </span>
+    </a>
+</div>
+
+<button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect vertical-menu-btn">
+    <i class="fa fa-fw fa-bars"></i>
+</button>
+
+<div data-simplebar class="sidebar-menu-scroll">
+
+    <!--- Sidemenu -->
+    <div id="sidebar-menu">
+        <!-- Left Menu Start -->
+        <ul class="metismenu list-unstyled" id="side-menu">
+            <li class="menu-title">Menu</li>
+
+            <li>
+                <a href="index.html">
+                    <i class="uil-home-alt"></i><span class="badge rounded-pill bg-primary float-end">01</span>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <i class="uil-window-section"></i>
+                    <span>Layouts</span>
+                </a>
+                <ul class="sub-menu" aria-expanded="true">
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow">Vertical</a>
+                        <ul class="sub-menu" aria-expanded="true">
+                            <li><a href="layouts-dark-sidebar.html">Dark Sidebar</a></li>
+                            <li><a href="layouts-compact-sidebar.html">Compact Sidebar</a></li>
+                            <li><a href="layouts-icon-sidebar.html">Icon Sidebar</a></li>
+                            <li><a href="layouts-boxed.html">Boxed Width</a></li>
+                            <li><a href="layouts-preloader.html">Preloader</a></li>
+                            <li><a href="layouts-colored-sidebar.html">Colored Sidebar</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow">Horizontal</a>
+                        <ul class="sub-menu" aria-expanded="true">
+                            <li><a href="layouts-horizontal.html">Horizontal</a></li>
+                            <li><a href="layouts-hori-topbar-dark.html">Topbar Dark</a></li>
+                            <li><a href="layouts-hori-boxed-width.html">Boxed Width</a></li>
+                            <li><a href="layouts-hori-preloader.html">Preloader</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+
+           
+        
+
+            <!-- <li>
+                <a href="javascript: void(0);" class="waves-effect">
+                    <i class="uil-shutter-alt"></i>
+                    <span class="badge rounded-pill bg-info float-end">9</span>
+                    <span>Forms</span>
+                </a>
+                <ul class="sub-menu" aria-expanded="false">
+                    <li><a href="form-elements.html">Basic Elements</a></li>
+                    <li><a href="form-validation.html">Validation</a></li>
+                    <li><a href="form-advanced.html">Advanced Plugins</a></li>
+                    <li><a href="form-editors.html">Editors</a></li>
+                    <li><a href="form-uploads.html">File Upload</a></li>
+                    <li><a href="form-xeditable.html">Xeditable</a></li>
+                    <li><a href="form-repeater.html">Repeater</a></li>
+                    <li><a href="form-wizard.html">Wizard</a></li>
+                    <li><a href="form-mask.html">Mask</a></li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <i class="uil-list-ul"></i>
+                    <span>Tables</span>
+                </a>
+                <ul class="sub-menu" aria-expanded="false">
+                    <li><a href="tables-basic.html">Bootstrap Basic</a></li>
+                    <li><a href="tables-datatable.html">Datatables</a></li>
+                    <li><a href="tables-responsive.html">Responsive</a></li>
+                    <li><a href="tables-editable.html">Editable</a></li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <i class="uil-chart"></i>
+                    <span>Charts</span>
+                </a>
+                <ul class="sub-menu" aria-expanded="false">
+                    <li><a href="charts-apex.html">Apex</a></li>
+                    <li><a href="charts-chartjs.html">Chartjs</a></li>
+                    <li><a href="charts-flot.html">Flot</a></li>
+                    <li><a href="charts-knob.html">Jquery Knob</a></li>
+                    <li><a href="charts-sparkline.html">Sparkline</a></li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <i class="uil-streering"></i>
+                    <span>Icons</span>
+                </a>
+                <ul class="sub-menu" aria-expanded="false">
+                    <li><a href="icons-unicons.html">Unicons</a></li>
+                    <li><a href="icons-boxicons.html">Boxicons</a></li>
+                    <li><a href="icons-materialdesign.html">Material Design</a></li>
+                    <li><a href="icons-dripicons.html">Dripicons</a></li>
+                    <li><a href="icons-fontawesome.html">Font Awesome</a></li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <i class="uil-location-point"></i>
+                    <span>Maps</span>
+                </a>
+                <ul class="sub-menu" aria-expanded="false">
+                    <li><a href="maps-google.html">Google</a></li>
+                    <li><a href="maps-vector.html">Vector</a></li>
+                    <li><a href="maps-leaflet.html">Leaflet</a></li>
+                </ul>
+            </li> -->
+
+            <!-- <li>
+                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <i class="uil-share-alt"></i>
+                    <span>Multi Level</span>
+                </a>
+                <ul class="sub-menu" aria-expanded="true">
+                    <li><a href="javascript: void(0);">Level 1.1</a></li>
+                    <li><a href="javascript: void(0);" class="has-arrow">Level 1.2</a>
+                        <ul class="sub-menu" aria-expanded="true">
+                            <li><a href="javascript: void(0);">Level 2.1</a></li>
+                            <li><a href="javascript: void(0);">Level 2.2</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>  -->
+
+        </ul>
+    </div>
+    <!-- Sidebar -->
+</div>
+</div>
+<!-- Left Sidebar End -->
