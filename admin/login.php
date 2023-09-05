33a79fd1e541edf602b1e788c138115b33bc998b
Emon <emon22205101732@diu.edu.bd>
2023-09-06 00:51:33 +0600

    update

---
+++ admin/login.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>Login page</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="rew">
+            <div class="col-md-6">
+                <div class="card">
+                 <h5 class="card-header">Login From</h5>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label >Email address</label>
+                                <input type="email" name="email" class="form-control">   
+                            </div>  <div class="form-group">
+                                <label >Password</label>
+                                <input type="password" name="password" class="form-control">   
+                            </div>
+                           <button type="submit" class="btn btn-success">Login</button>
+                           <a href="" class="btn btn-primary">Sign up</a>
+                           <a href="#" class="float-right">Forget your password ?</a>
+                        </form>
+                        
+
+                        <hr>
+                        <h5>Did not recieve your varification email ?
+                            <a href="#">Resend</a>
+                        </h5>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+  
+  </body>
+</html>
