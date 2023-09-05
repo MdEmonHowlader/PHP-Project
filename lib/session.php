bee5c1e935dadc3e3470e952f50336c87942a4ba
Emon <emon22205101732@diu.edu.bd>
2023-09-05 22:54:42 +0600

    update

--- lib/session.php
+++ lib/session.php
@@ -6,6 +6,13 @@ class Session{
     public static function set($key, $value){
         $_SESSION[$key]=$value;
     }
+    public static function get($key){
+        if(isset($_SESSION[$key])){
+            return  $_SESSION[$key];
+        }else{
+            return false;
+        }
+    }
 }
 
 
